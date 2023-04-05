e4796284330e768641ec7835945bf292e8beb557
Protyush Kumar Das <43172412+Protyush29@users.noreply.github.com>
2023-04-05 23:17:38 +0200

    Add files via upload
    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Static_website_search_tool
-Search tool for a website , given a json details regarding every page. Elasticsearch used for implementation.
+Search tool for a website , given a json details regarding every page. Elasticsearch used for implementation as a dependency. Hence it will be necessary to install it additionally. The steps below provides the complete process. For more info refer to https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-elasticsearch-on-ubuntu-20-04 .
+
+#Installation\
+Create a new conda environment and launch the app by following steps below:\
+
+conda create -n new_env python=3\
+conda activate new_env\
+
+curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -\
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list\
+sudo apt update\
+sudo apt install elasticsearch\
+
+pip install -r requirements.txt\
+
+sudo systemctl start elasticsearch\
+flask --app app run
+
+
+The server will be launched on http://127.0.0.1:5000  , open a browser to this address.
+
---
+++ app.py
@@ -0,0 +1,96 @@
+import csv
+import io
+import requests
+import json
+from elasticsearch import Elasticsearch
+#from config.config_handling import get_config_value
+from flask import Flask, jsonify, make_response, request, url_for, redirect, render_template
+from markupsafe import Markup
+
+app = Flask(__name__)
+index = "blog"
+
+def es_connection():
+	es_obj = Elasticsearch(hosts = "http://localhost:9200")
+	
+	if es_obj.ping():
+		print("Successfully connected to ES")
+		return es_obj
+	else:
+		print("Connection to ES unsuccessful")
+		return
+	
+	
+def es_ingestion(data):
+	client = es_connection()
+
+	if client:
+		print("Successfully ingested to ES")
+		ido = 1
+		for obj in data:
+			resp = client.index(index=index, id=ido, document=obj)
+			ido += 1
+			print (resp['result'])
+		return True
+	else:
+		print("Ingestion to ES unsuccessful")
+		return False
+	
+	
+    
+
+
+
+@app.route("/")
+def hello_world():
+    return render_template('result.html')
+
+    
+@app.route('/url_get', methods=['POST'])
+def url_get():
+    # Base parameters and form input
+    url = request.form['URL']
+    
+    # Make the API request and retrieve the JSON response
+    response = requests.get(url)
+    response_json = response.json()
+    
+    status = es_ingestion(response_json)
+    
+    if status:
+    	return render_template('result.html', disabled = 'false')
+    
+    else:
+    	print("Ingestion into es unsuccessful")
+    	return render_template('result.html', disabled = 'true') 
+    
+    
+@app.route('/query_results', methods=['POST'])
+def query_results():
+	# Base parameters and form input
+    	query = request.form['Query'].lower()
+    	client = es_connection()
+    	if not client:
+    	    return ('', 204)
+		
+    	client.indices.refresh(index=index)
+    	
+    	resp = client.search(index=index, query={"match_phrase": {"summary":query}}, highlight={"fields": {"*": {}}})
+    	matched_results = resp['hits']['total']['value']
+    	results = []
+    	
+    	print("Got %d Hits:" % resp['hits']['total']['value'])
+    	for hit in resp['hits']['hits']:
+    		title = Markup(str(hit["_source"]["title"]).lower().replace(query, "<mark>"+query+"</mark>"))
+    		tags = Markup(str(hit["_source"]["tags"]).lower().replace(query, "<mark>"+query+"</mark>"))
+    		summary = Markup(str(hit["_source"]["summary"]).lower().replace(query, "<mark>"+query+"</mark>"))
+    		print(summary)
+    		
+    		results.append([title,hit["_source"]["href"],tags,summary])
+    		
+    	return render_template('result.html',results=results,disabled = 'false')
+
+    
+
+if __name__ == '_main_':
+    app.run(debug=True)
